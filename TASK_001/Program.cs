d06b7be8b5831064fbe2acdbb8b848d595558ebe
Andrey Yakunin <a.e.yakunin@gmail.com>
2022-02-06 21:15:18 +0300

    Create project Task 001 Print the square of a number

---
+++ TASK_001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести квадрат числа
+
+
+Console.WriteLine("Введите число ->");
+
+string? numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+Console.WriteLine($"Квадрат числа: {number * number}");
