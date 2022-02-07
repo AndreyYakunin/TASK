f45894c1006dfa352ee13d1727354b1cc685e6b6
Andrey Yakunin <a.e.yakunin@gmail.com>
2022-02-07 07:44:40 +0300

    Create project TASK 005

---
+++ TASK_005/Program.cs
@@ -0,0 +1,18 @@
+﻿// 5. Найти максимальное из трех чисел 
+
+int[] numbers = { 168, 82, 38 };
+
+int GetMaxNumber(int[] array)
+{
+    int max = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (numbers[i] > max) max = numbers[i];
+    }
+    return max;
+}
+
+System.Console.WriteLine();
+int maxNumber = GetMaxNumber(numbers);
+System.Console.WriteLine($"Максимальное число: {maxNumber}");
+System.Console.WriteLine();
