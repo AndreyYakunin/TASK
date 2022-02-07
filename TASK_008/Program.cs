0bd620d5601e3680f88bd443f6b8f6b1b784ff63
Andrey Yakunin <a.e.yakunin@gmail.com>
2022-02-07 07:48:15 +0300

    Create project TASK 008

---
+++ TASK_008/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7. Показать числа от 1 до N
+Console.Clear();
+System.Console.Write("Введите число:   ");
+string writeNumber = Console.ReadLine();
+int endNumber = Convert.ToInt32(writeNumber);
+int Number = endNumber * (0);
+System.Console.Write($"{Number} ");
+
+while (Number < endNumber)
+{
+    Number++;
+    System.Console.Write($"{Number} ");
+}
+System.Console.WriteLine(); System.Console.WriteLine();
