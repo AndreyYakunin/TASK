a0c6343f1878d0e0be121e6039247d126ac7731d
Andrey Yakunin <a.e.yakunin@gmail.com>
2022-02-14 19:42:44 +0300

    Update project TASK 021

---
+++ TASK_021/Program.cs
@@ -0,0 +1,18 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+int InPut(string message)
+{   
+    Console.WriteLine(message);
+    string? num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+    }
+int numOne = InPut("Введите пятизначное число: ");
+
+bool valueOne = numOne % 10 == numOne / 1000;
+bool valueTwo = numOne % 100 == (numOne / 100) % 10;
+
+if (valueOne && valueTwo)
+Console.WriteLine("Число является палиндромом");
+
+else Console.WriteLine("Число не является палиндромом");
